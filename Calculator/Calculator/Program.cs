a21ef675777ac1780d1e68f6933388098783c313
mohammad alqutami <mohammedalqutami@yahoo.com>
2023-05-02 01:23:13 +0300

    done

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter first number");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("enter the second number");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine("The result is: ");
+Console.WriteLine(x + y);
+Console.WriteLine(x - y);
+Console.WriteLine(x * y);
+Console.WriteLine(x / y);
